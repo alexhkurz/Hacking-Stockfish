4c5aa916cbe8d2276284a1ebf070db6c1a60aba9
Alexander Kurz <alexhkurz@gmail.com>
2024-12-11 00:20:05 -0800

    a_score corrected

--- AnalyzeGame/analyze.py
+++ AnalyzeGame/analyze.py
@@ -93,11 +93,12 @@ class ChessAnalyzer:
                     sequence = info[i]["pv"]
                     score = info[i]["score"].white().score(mate_score=10000)
                     top_sequences.append((score, sequence))
+                print(); print(top_sequences); print()
 
                 # Calculate A-score
                 best_score = top_sequences[0][0]
                 fifth_score = top_sequences[4][0] if len(top_sequences) >= 5 else best_score
-                a_score = abs((best_score - fifth_score) / best_score) if best_score != 0 else 0
+                a_score = abs(best_score - fifth_score) if best_score != 0 else 0
 
                 # Calculate B-score
                 b_score = None
@@ -177,8 +178,8 @@ class ChessAnalyzer:
 
 def main():
     #pgn_string = "1.e4 c5 2.Nf3 a6 3.d3 g6 4.g3 Bg7 5.Bg2 b5 6.O-O Bb7 7.c3 e5 8.a3 Ne7 9.b4 d6 10.Nbd2 O-O 11.Nb3 Nd7 12.Be3 Rc8 13.Rc1 h6 14.Nfd2 f5 15.f4 Kh7 16.Qe2 cxb4 17.axb4 exf4 18.Bxf4 Rxc3 19.Rxc3 Bxc3 20.Bxd6 Qb6+ 21.Bc5 Nxc5 22.bxc5 Qe6 23.d4 Rd8 24.Qd3 Bxd2 25.Nxd2 fxe4 26.Nxe4 Nf5 27.d5 Qe5 28.g4 Ne7 29.Rf7+ Kg8 30.Qf1 Nxd5 31.Rxb7 Qd4+ 32.Kh1 Rf8 33.Qg1 Ne3 34.Re7 a5 35.c6 a4 36.Qxe3 Qxe3 37.Nf6+ Rxf6 38.Rxe3 Rd6 39.h4 Rd1+ 40.Kh2 b4 41.c7  1-0"
-    #pgn_string = "1. e4 f6 2. d4 g5 3. Qh5#"
-    pgn_string = "1. d4 d5 2. c4 Nf6 { D06 Queen's Gambit Declined: Marshall Defense } 3. cxd5 Nxd5 4. e4 Nf6 5. Nc3 e6 6. Nf3 Bb4 7. Bd3 Bxc3+ 8. bxc3 O-O 9. O-O b6 10. Bg5 Nbd7 11. e5 h6 12. Bxh6 gxh6 13. exf6 Qxf6 14. Be4 Rb8 15. Qa4 a5 16. Bc6 Rd8 17. Rae1 Nf8 18. Ne5 Bb7 19. Re3 Bxc6 20. Nxc6 Rdc8 21. Nxb8 Rxb8 22. Qb5 Ng6 23. Rf3 Qe7 24. Qc6 Rd8 25. Qe4 Rd5 26. Rg3 Rg5 27. Rxg5 Qxg5 28. Re1 Qd2 29. Qe3 Qxa2 30. Qxh6 Qc2 31. Qe3 a4 32. g3 a3 33. Re2 Qb1+ 34. Re1 Qb3 35. Qc1 a2 36. Kg2 Ne7 37. Re2 Nd5 38. Qa1 Nxc3 39. Re3 Qb1 40. Qxc3 a1=Q { Black wins on time. } 0-1"
+    pgn_string = "1. e4 f6 2. d4 g5 3. Qh5#"
+    #pgn_string = "1. d4 d5 2. c4 Nf6 { D06 Queen's Gambit Declined: Marshall Defense } 3. cxd5 Nxd5 4. e4 Nf6 5. Nc3 e6 6. Nf3 Bb4 7. Bd3 Bxc3+ 8. bxc3 O-O 9. O-O b6 10. Bg5 Nbd7 11. e5 h6 12. Bxh6 gxh6 13. exf6 Qxf6 14. Be4 Rb8 15. Qa4 a5 16. Bc6 Rd8 17. Rae1 Nf8 18. Ne5 Bb7 19. Re3 Bxc6 20. Nxc6 Rdc8 21. Nxb8 Rxb8 22. Qb5 Ng6 23. Rf3 Qe7 24. Qc6 Rd8 25. Qe4 Rd5 26. Rg3 Rg5 27. Rxg5 Qxg5 28. Re1 Qd2 29. Qe3 Qxa2 30. Qxh6 Qc2 31. Qe3 a4 32. g3 a3 33. Re2 Qb1+ 34. Re1 Qb3 35. Qc1 a2 36. Kg2 Ne7 37. Re2 Nd5 38. Qa1 Nxc3 39. Re3 Qb1 40. Qxc3 a1=Q { Black wins on time. } 0-1"
     analyzer = ChessAnalyzer()
     analyzer.analyzeGame(pgn_string)
     analyzer.close()
